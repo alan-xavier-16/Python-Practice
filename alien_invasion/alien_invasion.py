48a103ecb4f880a9c47f10a88770a95f585c2cb4
Alan Xavier <alan_xvr@hotmail.com>
2020-06-27 15:45:56 -0400

    Start building a game screen and a ship 1. alien_invasion.py   - Import 'settings' from 'Settings' class   - Import 'ship' from 'Ship' class   - Initiate a 'settings' attribute from 'Settings'   - Initiate 'screen' attribute using 'pygame' with 'width' & 'height' from 'settings' instance   - Initiate 'ship' attribute as an instance of 'Ship' 2. settings.py   - Contains attributes for:     - Screen width & height     - Background color 3. ship.py   - __init__     - 'screen' attribute is made equal to the instance of the 'ai_game'     - 'image' attribute is set to a bitmap image     - Use 'pygame' to treat the screen & ship as 'rectangles'     - 'rect' attribute 'midbottom' property is set to equal the midbottom of the screen_rect on initial load   - blitme method     - Position 'ship' at the location specified

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -8,8 +10,11 @@ class AlienInvasion:
     def __init__(self):
         """Initialize game and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start main game loop"""
@@ -18,6 +23,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw screen with bg colour and other resources
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Show new screen
             pygame.display.flip()
