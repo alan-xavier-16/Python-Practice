19e1ca279677f1bbc35c091a3417448fa1875558
Alan Xavier <alan_xvr@hotmail.com>
2020-06-27 16:24:36 -0400

    Allow ship to move left or right 1. ship.py   - 'moving_right' & 'moving_left' flags are False by default   - 'update' method     - If 'moving_right' or 'moving_left' are TRUE: Move ship by 1 pixel 2. alien_invasion.py   - Call 'ship.update()' method between '_check_events' & '_update_screen' methods   - _check_events Method:     - Use 'pygame' to check for 'keypress' events     - Alter 'ship.moving_left' or 'shop.moving_right' based on event

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -15,6 +15,17 @@ class Ship:
         # Start each new ship at the bottom center
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flags for continuous motion
+        self.moving_right = False
+        self.moving_left = False
+
     def blitme(self):
         """Draw ship at current location"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship position based on movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
